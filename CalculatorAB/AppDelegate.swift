f8f73b8d00b2011b85b9baab23ce70a4f329c8a9
Aidan <drwhovian@gmail.com>
2019-01-31 13:16:14 +1300

    First Commit

---
+++ CalculatorAB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CalculatorAB
+//
+//  Created by Aidan Brock on 31/01/2019.
+//  Copyright © 2019 Aidan Brock. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CalculatorAB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CalculatorAB
+//
+//  Created by Aidan Brock on 31/01/2019.
+//  Copyright © 2019 Aidan Brock. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
